ed6e4ed4855d28db187e2671166b7c7dd0c131c7
shubham <shubham22121@gmail.com>
2022-08-22 17:13:54 +0530

    build: mongo auth

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
+
+const {
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_IP,
+    MONGO_PORT,
+   
+  } = require("./config/config");
 
 app.get('/', (req, res)=>{
     res.send("hello from docker1!")
 });
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("succesfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
 
 const port = process.env.PORT || 3000;
 
